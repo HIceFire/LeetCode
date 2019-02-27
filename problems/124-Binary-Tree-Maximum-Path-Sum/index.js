5bdaf84c7f27448bee329a41a1a85aee2c0ed96d
vq0599 <vq0599@outlook.com>
2019-02-27 18:54:46 +0800

    New 124

---
+++ problems/124-Binary-Tree-Maximum-Path-Sum/index.js
@@ -0,0 +1,64 @@
+/*
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * Given a non-empty binary tree, find the maximum path sum.
+ * 
+ * For this problem, a path is defined as any sequence of nodes from some
+ * starting node to any node in the tree along the parent-child connections.
+ * The path must contain at least one node and does not need to go through the
+ * root.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3]
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   3
+ * 
+ * Output: 6
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [-10,9,20,null,null,15,7]
+ * 
+ * -10
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 
+ * Output: 42
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+  let max = Number.NEGATIVE_INFINITY
+
+  const calc = (root) => {
+    if (!root) return 0
+
+    const left = Math.max(calc(root.left), 0)
+    const right = Math.max(calc(root.right), 0)
+    max = Math.max(max, left.val + right.val + root.val)
+    return root.val + Math.max(left, right)
+  }
+
+  helper(root)
+  return max
+};
+
